d490f777ba9f18bcd915071a51eccb7ca0346443
SchuÃgnel, André <andre.schuengel@we-online.de>
2023-06-14 10:00:28 +0200

    Added default route

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace aschuengel_web_app_sample.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
